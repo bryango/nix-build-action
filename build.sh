43d82599f702fe8170deea47a3acf66513227d2e
bryango <bryango@users.noreply.github.com>
2023-10-01 16:37:24 +0800

    Fix typo in url

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")" || exit
 git config --global user.email "bryanlais@gmail.com"
 git config --global user.name "Bryan Lai"
 
-git clone --filter=blob:none --branch=master --single-branch --verbose https://github.com/nixpkgs/nixpkgs.git
+git clone --filter=blob:none --branch=master --single-branch --verbose https://github.com/NixOS/nixpkgs.git
 cd nixpkgs || exit
 
 nix build .#arrow-cpp "$@" || true
