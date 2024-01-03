27e6a935af5ee2101787d06c064b365ddeebd4c5
Bryan Lai <bryanlais@gmail.com>
2024-01-03 13:43:12 +0800

    Update to test PR #278410

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ git config --global user.name "Bryan Lai"
 git clone --filter=blob:none --branch=master --single-branch --verbose https://github.com/NixOS/nixpkgs.git
 cd nixpkgs || exit
 
-nix-shell -p nixpkgs-review --run "nixpkgs-review pr --build-args='$*' --no-shell --print-result 273740" || true
+nix-shell -p nixpkgs-review --run "nixpkgs-review pr --build-args='$*' --no-shell --print-result 278410" || true
 
