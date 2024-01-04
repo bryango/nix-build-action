b04b0ba774ffe6e1e8d32f122b020f224a64f7b9
Bryan Lai <bryanlais@gmail.com>
2024-01-04 17:15:57 +0800

    Add & link new build: folly-multi-outputs#watchman

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,11 +4,8 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-git config --global user.email "bryanlais@gmail.com"
-git config --global user.name "Bryan Lai"
-
-git clone --filter=blob:none --branch=master --single-branch --verbose https://github.com/NixOS/nixpkgs.git
+git clone --filter=blob:none --branch=folly-multi-outputs --single-branch --verbose https://github.com/bryango/nixpkgs.git
 cd nixpkgs || exit
 
-nix-shell -p nixpkgs-review --run "nixpkgs-review pr --build-args='$*' --no-shell --print-result 278410" || true
+nix build .#watchman "$@"
 
