6aa47ce4e48f6b45bd5f8507007966fdbe9abaae
Bryan Lai <bryanlais@gmail.com>
2024-01-04 17:06:46 +0800

    Restructure to allow different build scripts

---
+++ build-pr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# build home configs
+
+set -x
+cd "$(dirname "$0")" || exit
+
+git config --global user.email "bryanlais@gmail.com"
+git config --global user.name "Bryan Lai"
+
+git clone --filter=blob:none --branch=master --single-branch --verbose https://github.com/NixOS/nixpkgs.git
+cd nixpkgs || exit
+
+nix-shell -p nixpkgs-review --run "nixpkgs-review pr --build-args='$*' --no-shell --print-result 278410" || true
+
