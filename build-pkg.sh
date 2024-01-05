23a017b9b528e5dc1587dfd533f506a262847c6e
bryango <bryango@users.noreply.github.com>
2024-01-05 23:29:32 +0800

    Build libuv with logs

--- build-pkg.sh
+++ build-pkg.sh
@@ -7,5 +7,4 @@ cd "$(dirname "$0")" || exit
 git clone --filter=blob:none --branch=folly-multi-outputs --single-branch --verbose https://github.com/bryango/nixpkgs.git
 cd nixpkgs || exit
 
-nix build .#watchman "$@"
-
+nix build --print-build-logs .#libuv "$@"
