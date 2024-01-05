19d4d23e525ff98365b546fedb4d77eb204936fb
bryango <bryango@users.noreply.github.com>
2024-01-06 01:14:14 +0800

    Simplify build-pkg.sh & build watchman

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,7 +4,4 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-git clone --filter=blob:none --branch=folly-multi-outputs --single-branch --verbose https://github.com/bryango/nixpkgs.git
-cd nixpkgs || exit
-
-nix build --print-build-logs .#libuv "$@"
+nix build --print-build-logs github:bryango/nixpkgs/folly-multi-outputs#watchman "$@"
