b43d6f3f9514557b79a65f840d626c24d011e1d4
bryango <bryango@users.noreply.github.com>
2024-01-06 14:40:34 +0800

    Update build-pkg.sh

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,6 +4,5 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-# nix build --print-build-logs github:NixOS/nixpkgs/staging#watchman "$@"
-
-nix why-depends github:NixOS/nixpkgs/staging#{stdenv,python3} "$@" || true
+nix build --print-build-logs github:NixOS/nixpkgs/staging#stdenv "$@"
+nix why-depends --all --precise github:NixOS/nixpkgs/staging#{stdenv,python3} "$@" || true
