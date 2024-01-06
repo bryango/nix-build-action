80d7513eacd561da98598852b22cb44477951e18
bryango <bryango@users.noreply.github.com>
2024-01-06 15:53:48 +0800

    Fix build-pkg.sh

--- build-pkg.sh
+++ build-pkg.sh
@@ -7,4 +7,4 @@ cd "$(dirname "$0")" || exit
 flakeref=github:NixOS/nixpkgs/nixpkgs-unstable
 
 nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
-nix why-depends --all --precise ./result .#python3.out || true
+nix why-depends --all --precise ./result "$flakeref"#python3.out || true
