6251cd70ebb5a21ed37ce0bdc13a883c935820ac
bryango <bryango@users.noreply.github.com>
2024-01-06 19:26:32 +0800

    Rebuild HEAD^ of pr 278643

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,8 +4,8 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-# https://hydra.nixos.org/jobset/nixpkgs/staging-next
-flakeref=github:NixOS/nixpkgs/4a887669bd93a80b40111cd337745d33be35e046
+# HEAD^ of pr 278643
+flakeref=github:NixOS/nixpkgs/10b0f19e0d6d4998e4e8e6bea70664ae8923d7cf
 
 # nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
 # nix why-depends --all --precise ./result "$flakeref"#python3.out || true
