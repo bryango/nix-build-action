2dbf98708b69e54409687064110bdbba00218b9f
bryango <bryango@users.noreply.github.com>
2024-01-06 21:27:00 +0800

    Test build after merge into staging-next

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,10 +4,10 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-# HEAD^ of pr 278643
-flakeref=github:NixOS/nixpkgs/10b0f19e0d6d4998e4e8e6bea70664ae8923d7cf
+# Merged HEAD of 278643 into cached staging-next
+flakeref=github:NixOS/bryango/staging-next-folly
 
-nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
-nix why-depends --all --precise ./result "$flakeref"#python3.out
+# nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
+# nix why-depends --all --precise ./result "$flakeref"#python3.out
 
 nix build --print-build-logs "$flakeref#watchman" "$@" || true
