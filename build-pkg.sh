76e5633629fae9a01b41f803b13a9db916e63279
bryango <bryango@users.noreply.github.com>
2024-01-06 15:58:45 +0800

    Check last successful staging-next

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,7 +4,7 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-flakeref=github:NixOS/nixpkgs/nixpkgs-unstable
+flakeref=github:NixOS/nixpkgs/e61ae81f8f19d851c3b33e562198dd8f80307f36
 
 nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
 nix why-depends --all --precise ./result "$flakeref"#python3.out || true
