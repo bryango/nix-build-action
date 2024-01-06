8710404f6c206a74c8354493fd6ef6861b44e3bb
bryango <bryango@users.noreply.github.com>
2024-01-06 18:45:15 +0800

    Check next wip staging-next

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,7 +4,8 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-flakeref=github:NixOS/nixpkgs/e61ae81f8f19d851c3b33e562198dd8f80307f36
+# https://hydra.nixos.org/jobset/nixpkgs/staging-next
+flakeref=github:NixOS/nixpkgs/887e96e875e933c0a5d203ffc40e202f3d16317e
 
 nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
 nix why-depends --all --precise ./result "$flakeref"#python3.out || true
