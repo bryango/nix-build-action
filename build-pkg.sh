b0c9b8ffd0295695d0df4394902d590581f09a32
bryango <bryango@users.noreply.github.com>
2024-01-06 18:58:30 +0800

    Build watchman at this point

--- build-pkg.sh
+++ build-pkg.sh
@@ -7,5 +7,7 @@ cd "$(dirname "$0")" || exit
 # https://hydra.nixos.org/jobset/nixpkgs/staging-next
 flakeref=github:NixOS/nixpkgs/f3c04dc97a71c8c3e650b4cf1602ec5c26ac1cf1
 
-nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
-nix why-depends --all --precise ./result "$flakeref"#python3.out || true
+# nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
+# nix why-depends --all --precise ./result "$flakeref"#python3.out || true
+
+nix build --print-build-logs "$flakeref#watchman" "$@"
