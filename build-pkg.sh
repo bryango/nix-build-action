44fd13c74084efc4f2554f70d79cf01e23d1506e
bryango <bryango@users.noreply.github.com>
2024-01-06 19:28:01 +0800

    Build dev env

--- build-pkg.sh
+++ build-pkg.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")" || exit
 # HEAD^ of pr 278643
 flakeref=github:NixOS/nixpkgs/10b0f19e0d6d4998e4e8e6bea70664ae8923d7cf
 
-# nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
-# nix why-depends --all --precise ./result "$flakeref"#python3.out || true
+nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
+nix why-depends --all --precise ./result "$flakeref"#python3.out
 
-nix build --print-build-logs "$flakeref#watchman" "$@"
+nix build --print-build-logs "$flakeref#watchman" "$@" || true
