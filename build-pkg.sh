cdf92b6b7041557e0ac45fa506398d1ddacbb35f
bryango <bryango@users.noreply.github.com>
2024-01-06 15:21:46 +0800

    Update build-pkg.sh

--- build-pkg.sh
+++ build-pkg.sh
@@ -5,5 +5,5 @@ set -x
 cd "$(dirname "$0")" || exit
 
 flakeref=github:NixOS/nixpkgs/staging
-nix build --print-build-logs --impure "with import ./. {}; mkShell { inputsFrom = [ watchman ]; }" "$@"
+nix build --print-build-logs --impure --expr "with import ./. {}; mkShell { inputsFrom = [ watchman ]; }" "$@"
 nix why-depends --all --precise ./result "$flakeref"#python3.out || true
