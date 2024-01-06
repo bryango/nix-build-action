197c6872371dfaefe0fadba0f9fac0053957db31
bryango <bryango@users.noreply.github.com>
2024-01-06 09:20:36 +0800

    Check if watchman builds on HEAD^

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,4 +4,5 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-nix build --print-build-logs github:bryango/nixpkgs/folly-multi-outputs#watchman "$@"
+# git log folly-multi-outputs^
+nix build --print-build-logs github:bryango/nixpkgs/10b0f19e0d6d4998e4e8e6bea70664ae8923d7cf#watchman "$@"
