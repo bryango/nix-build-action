eff34af6a8eef2205de1a070c85648df296e08a7
bryango <bryango@users.noreply.github.com>
2024-01-06 12:30:02 +0800

    Build watchman on staging-next

--- build-pkg.sh
+++ build-pkg.sh
@@ -5,4 +5,4 @@ set -x
 cd "$(dirname "$0")" || exit
 
 # git log folly-multi-outputs^
-nix build --print-build-logs github:bryango/nixpkgs/10b0f19e0d6d4998e4e8e6bea70664ae8923d7cf#watchman "$@"
+nix build --print-build-logs github:NixOS/nixpkgs/staging-next#watchman "$@"
