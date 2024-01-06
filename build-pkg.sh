22e4952fefd0ddef6f0b1edabd8825ad397daf9a
bryango <bryango@users.noreply.github.com>
2024-01-06 12:43:52 +0800

    Update build-pkg.sh

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,5 +4,4 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-# git log folly-multi-outputs^
-nix build --print-build-logs github:NixOS/nixpkgs/staging-next#watchman "$@"
+nix build --print-build-logs github:NixOS/nixpkgs/staging#watchman "$@"
