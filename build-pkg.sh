6523fd309f3683ad2496032ee998d9be20d237eb
bryango <bryango@users.noreply.github.com>
2024-01-06 14:44:59 +0800

    Fix why-depends cli args

--- build-pkg.sh
+++ build-pkg.sh
@@ -5,4 +5,4 @@ set -x
 cd "$(dirname "$0")" || exit
 
 nix build --print-build-logs github:NixOS/nixpkgs/staging#stdenv "$@"
-nix why-depends --all --precise github:NixOS/nixpkgs/staging#{stdenv,python3} "$@" || true
+nix why-depends --all --precise github:NixOS/nixpkgs/staging#{stdenv,python3.out} || true
