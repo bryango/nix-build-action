c2744bc48ebd11138722e45b9e49f55439402f55
bryango <bryango@users.noreply.github.com>
2024-01-06 14:50:12 +0800

    Check python3 deps in nixpkgs-unstable

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,5 +4,6 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-nix build --print-build-logs github:NixOS/nixpkgs/staging#stdenv "$@"
-nix why-depends --all --precise github:NixOS/nixpkgs/staging#{stdenv,python3.out} || true
+flakeref=github:NixOS/nixpkgs/nixpkgs-unstable
+nix build --print-build-logs "$flakeref"#stdenv "$@"
+nix why-depends --all --precise "$flakeref"#{stdenv,python3.out} || true
