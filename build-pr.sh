b4efe21578d9ca9132be074ab06299d464efb7af
bryango <bryango@users.noreply.github.com>
2024-01-30 09:57:03 +0800

    Review 280617

--- build-pr.sh
+++ build-pr.sh
@@ -10,4 +10,4 @@ git config --global user.name "Bryan Lai"
 git clone --filter=blob:none --branch=master --single-branch --verbose https://github.com/NixOS/nixpkgs.git
 cd nixpkgs || exit
 
-nix-shell -p nixpkgs-review --run "nixpkgs-review pr --build-args='$*' --no-shell --print-result 283666" || true
+nix-shell -p nixpkgs-review --run "nixpkgs-review pr --build-args='$*' --no-shell --print-result 280617" || true
