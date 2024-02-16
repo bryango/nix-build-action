5c3a8407bfe8fe99a0bc17593c339a9dccdd3268
bryango <bryango@users.noreply.github.com>
2024-02-17 00:30:44 +0800

    Test git-branchless with man pages

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,7 +4,7 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-flakeref=github:bryango/pinix
+flakeref=github:bryango/nixpkgs/6b198ef440e7328ed85da37daf042e8f964ec07a#git-branchless
 
 # nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
 # nix why-depends --all --precise ./result "$flakeref"#python3.out
