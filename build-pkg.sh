871f05e2fe31541821288191d1bb0b15db8bb39a
Bryan Lai <bryanlais@gmail.com>
2024-03-13 23:55:13 +0800

    Fetch the necessary license from cachix

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,6 +4,10 @@
 set -x
 cd "$(dirname "$0")" || exit
 
+## fetch the necessary license from store
+nix copy --from https://chezbryan.cachix.org \
+    /nix/store/xzddkr1n8s5rpbwz0s2n1b2a2wyj010p-license.tar.gz
+
 # flakeref=github:NixOS/nixpkgs#pkgsCross.raspberryPi.git-branchless
 flakeref=.
 
