af7b2239edaf94fb9da1b84e8745d0480aa2fc73
Bryan Lai <bryanlais@gmail.com>
2024-03-16 22:42:38 +0800

    Test python-lsp-black fix 296433 on staging-next

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,12 +4,12 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-## fetch the necessary license from store
-nix copy --from https://chezbryan.cachix.org --no-check-sigs \
-    /nix/store/xzddkr1n8s5rpbwz0s2n1b2a2wyj010p-license.tar.gz
+# ## fetch the necessary license from store
+# nix copy --from https://chezbryan.cachix.org --no-check-sigs \
+#     /nix/store/xzddkr1n8s5rpbwz0s2n1b2a2wyj010p-license.tar.gz
 
 # flakeref=github:NixOS/nixpkgs#pkgsCross.raspberryPi.git-branchless
-flakeref=.
+flakeref=github:NixOS/nixpkgs/17840d41a7cd2fa044d2205e2c5c38bd673f3b36
 
 # nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
 # nix why-depends --all --precise ./result "$flakeref"#python3.out
