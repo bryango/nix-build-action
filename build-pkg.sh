00affdcddf400db3ccaa0450f9e0eb80ec8b0131
Bryan Lai <bryanlais@gmail.com>
2025-02-27 23:52:38 +0800

    Test newt / snack build for darwin

--- build-pkg.sh
+++ build-pkg.sh
@@ -11,7 +11,8 @@ cd "$(dirname "$0")" || exit
 # flakeref=( .#pulsar )
 # flakeref=github:NixOS/nixpkgs#pkgsCross.raspberryPi.git-branchless
 # flakeref=( github:bryango/nixpkgs/dev#rust-cbindgen )
-flakeref=( 'github:NixOS/nixpkgs?ref=pull/384706/merge#'{tectonic,texpresso} )
+# flakeref=( 'github:NixOS/nixpkgs?ref=pull/384706/merge#'{tectonic,texpresso} )
+flakeref=( github:bryango/nixpkgs/newt#python3Packages.snack )
 
 # nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
 # nix why-depends --all --precise ./result "$flakeref"#python3.out
