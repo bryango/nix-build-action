8d94a8f3b732e212ff2c323feae1fdee4a073ed5
Bryan Lai <bryanlais@gmail.com>
2025-03-01 17:42:32 +0800

    Test tectonic unstable

--- build-pkg.sh
+++ build-pkg.sh
@@ -12,7 +12,7 @@ cd "$(dirname "$0")" || exit
 # flakeref=github:NixOS/nixpkgs#pkgsCross.raspberryPi.git-branchless
 # flakeref=( github:bryango/nixpkgs/dev#rust-cbindgen )
 # flakeref=( 'github:NixOS/nixpkgs?ref=pull/384706/merge#'{tectonic,texpresso} )
-flakeref=( github:bryango/nixpkgs/dev#python3Packages.snack.passthru.tests.pythonModule )
+flakeref=( github:bryango/nixpkgs/tectonic#tectonic.passthru.tests.biber-compatibility )
 
 # nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
 # nix why-depends --all --precise ./result "$flakeref"#python3.out
