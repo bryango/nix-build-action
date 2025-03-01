1439908cd120795fa8fff66920f8a28e218ffaa0
Bryan Lai <bryanlais@gmail.com>
2025-03-01 18:06:30 +0800

    Add more tectonic tests

--- build-pkg.sh
+++ build-pkg.sh
@@ -12,7 +12,7 @@ cd "$(dirname "$0")" || exit
 # flakeref=github:NixOS/nixpkgs#pkgsCross.raspberryPi.git-branchless
 # flakeref=( github:bryango/nixpkgs/dev#rust-cbindgen )
 # flakeref=( 'github:NixOS/nixpkgs?ref=pull/384706/merge#'{tectonic,texpresso} )
-flakeref=( github:bryango/nixpkgs/tectonic#tectonic.passthru.tests.biber-compatibility )
+flakeref=( github:bryango/nixpkgs/tectonic#tectonic.passthru.tests.{biber-compatibility,workspace,nextonic} )
 
 # nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
 # nix why-depends --all --precise ./result "$flakeref"#python3.out
