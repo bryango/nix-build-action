77c66fcfdc6a2769e444e5b704a824629ed73203
Bryan Lai <bryanlais@gmail.com>
2025-07-05 18:27:44 +0800

    Test zed-editor fix

--- build-pkg.sh
+++ build-pkg.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")" || exit
 # nix copy --from https://chezbryan.cachix.org --no-check-sigs \
 #     /nix/store/xzddkr1n8s5rpbwz0s2n1b2a2wyj010p-license.tar.gz
 
-flakeref=( .#pulsar )
+# flakeref=( .#pulsar )
 # flakeref=github:NixOS/nixpkgs#pkgsCross.raspberryPi.git-branchless
 # flakeref=( github:bryango/nixpkgs/dev#rust-cbindgen )
 # flakeref=( 'github:NixOS/nixpkgs?ref=pull/384706/merge#'{tectonic,texpresso} )
@@ -16,6 +16,9 @@ flakeref=( .#pulsar )
 #     github:bryango/nixpkgs/tectonic#tectonic
 #     github:bryango/nixpkgs/tectonic#tectonic.passthru.tests.{biber-compatibility,workspace,nextonic}
 # )
+flakeref=(
+    'github:NixOS/nixpkgs?ref=pull/422680/merge#zed-editor'
+)
 
 # nix build --print-build-logs --impure --expr 'with import (builtins.getFlake "'"$flakeref"'") {}; mkShell { inputsFrom = [ watchman ]; }' "$@"
 # nix why-depends --all --precise ./result "$flakeref"#python3.out
